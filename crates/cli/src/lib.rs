183d42492ab489fbf645fd8e8fd82e0c83fc9241
Steve Klabnik <steve@steveklabnik.com>
2023-07-12 17:06:19 -0500

    integrate anyhow
    as your application grows here, I think you'll come to appreciate
    anyhow. A common suggestion is "thiserr for libraries, anyhow for
    applications," and so we're following that. You're effectively putting
    main in the library, which isn't bad, but that's why this is not
    *strictly* following the letter of that advice, but instead, the spirit.
    
    .context will give you output that looks like this:
    
    ```
    Error: current directory should exist
    
    Caused by:
        No such file or directory (os error 2)
    ```
    
    I also tweaked the error messages, given that it not existing is the
    only issue. For example, maybe the problem is permissions, in which
    case, the error would look like this:
    
    ```
    Error: current directory should exist
    
    Caused by:
        Permission deined (os error 13)
    ```
    
    which is uh, wrong. So as of this commit, this will instead say
    
    ```
    Error: problem fetching current directory
    
    Caused by:
        Permission deined (os error 13)
    ```
    
    or
    
    ```
    Error: problem fetching current directory
    
    Caused by:
        No such file or directory (os error 2)
    ```
    
    The other two messages are already pretty good, I'd say.

--- crates/cli/src/lib.rs
+++ crates/cli/src/lib.rs
@@ -1,22 +1,24 @@
 mod commands;
 
+use anyhow::{Context, Result};
 use pacquet_package_json::PackageJson;
 use pacquet_registry::RegistryManager;
 
 use crate::commands::get_commands;
 
-pub async fn run_commands() {
+pub async fn run_commands() -> Result<()> {
     let matches = get_commands().get_matches();
-    let current_directory = std::env::current_dir().expect("current directory should exist");
+    let current_directory =
+        std::env::current_dir().context("problem fetching current directory")?;
     let cache_directory = current_directory.join(".pacquet").as_path().to_owned();
     let node_modules = current_directory.join("node_modules").as_path().to_owned();
 
     if !cache_directory.exists() {
-        std::fs::create_dir(&cache_directory).expect("cache folder creation failed");
+        std::fs::create_dir(&cache_directory).context("cache folder creation failed")?;
     }
 
     if !node_modules.exists() {
-        std::fs::create_dir(&node_modules).expect("node_modules folder creation failed");
+        std::fs::create_dir(&node_modules).context("node_modules folder creation failed")?;
     }
 
     let mut registry_manager = RegistryManager::new(cache_directory);
@@ -29,4 +31,6 @@ pub async fn run_commands() {
         let pkg = PackageJson::from_current_directory();
         pkg.create_if_needed();
     }
+
+    Ok(())
 }
