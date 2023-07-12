a19e6614f8bb6448c54c985fa7523be398e5ce53
Yagiz Nizipli <yagiz@nizipli.com>
2023-07-12 17:45:38 -0400

    add init command

--- crates/package_json/src/lib.rs
+++ crates/package_json/src/lib.rs
@@ -1,11 +1,13 @@
 use std::{collections::HashMap, env, ffi::OsStr, io::Write, path::PathBuf};
 
-struct PackageJson {
+use serde_json;
+
+pub struct PackageJson {
     path: PathBuf,
 }
 
 impl PackageJson {
-    pub fn new() -> Self {
+    pub fn from_current_directory() -> Self {
         PackageJson {
             path: env::current_dir()
                 .expect("current directory should exist")
@@ -14,19 +16,39 @@ impl PackageJson {
         }
     }
 
+    pub fn from_path(path: PathBuf) -> Self {
+        PackageJson { path }
+    }
+
     pub fn create_if_needed(&self) {
         if self.path.exists() {
             return;
         }
 
-        let folder_name = self.path.file_name().unwrap_or(OsStr::new(""));
+        let folder_name = self
+            .path
+            .parent()
+            .expect("should have a parent folder")
+            .file_name()
+            .unwrap_or(OsStr::new(""));
 
-        let mut file = std::fs::File::open(&self.path).unwrap();
+        let mut file = std::fs::File::create(&self.path).unwrap();
+        let empty_object: HashMap<String, String> = HashMap::new();
+        let mut scripts = HashMap::new();
+        scripts.insert("test", "echo \"Error: no test specified\" && exit 1");
 
         let mut contents = HashMap::new();
-        contents.insert("name", folder_name.to_str().unwrap());
+        contents.insert("name", serde_json::to_value(folder_name.to_str().unwrap()).unwrap());
+        contents.insert("version", serde_json::to_value("1.0.0").unwrap());
+        contents.insert("description", serde_json::to_value("").unwrap());
+        contents.insert("main", serde_json::to_value("index.js").unwrap());
+        contents.insert("author", serde_json::to_value("").unwrap());
+        contents.insert("license", serde_json::to_value("MIT").unwrap());
+        contents.insert("scripts", serde_json::to_value(&scripts).unwrap());
+        contents.insert("dependencies", serde_json::to_value(&empty_object).unwrap());
+        contents.insert("devDependencies", serde_json::to_value(&empty_object).unwrap());
 
-        let serialized = serde_json::to_string(&contents).unwrap();
+        let serialized = serde_json::to_string_pretty(&contents).unwrap();
         file.write_all(serialized.as_bytes()).unwrap();
     }
 }
