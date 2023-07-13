46143582327c021b4a1a8963c119d5af753cdf20
Steve Klabnik <steve@steveklabnik.com>
2023-07-13 14:29:27 -0500

    Use #[from]
    thiserror provides a way to generate from implementations. This commit
    demonstrates using this feature. Nice catch, @weihanglo!

--- crates/registry/src/error.rs
+++ crates/registry/src/error.rs
@@ -7,21 +7,9 @@ pub enum RegistryError {
     #[error("missing version `{0}` on package `${0}`")]
     MissingVersionRelease(String, String),
     #[error("network error while downloading `${0}`")]
-    Network(reqwest::Error),
+    Network(#[from] reqwest::Error),
     #[error("io error `${0}`")]
-    Io(std::io::Error),
+    Io(#[from] std::io::Error),
     #[error("serialization failed: `${0}")]
     Serialization(String),
-}
-
-impl From<std::io::Error> for RegistryError {
-    fn from(value: std::io::Error) -> Self {
-        RegistryError::Io(value)
-    }
-}
-
-impl From<reqwest::Error> for RegistryError {
-    fn from(value: reqwest::Error) -> Self {
-        RegistryError::Network(value)
-    }
 }
