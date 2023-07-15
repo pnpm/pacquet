ddab69ed4da377f43bb726107fc697b49a8e898a
Yagiz Nizipli <yagiz@nizipli.com>
2023-07-15 15:16:22 -0400

    fix: improve package.json serialization

--- crates/cli/src/lib.rs
+++ crates/cli/src/lib.rs
@@ -10,17 +10,19 @@ use pacquet_registry::RegistryManager;
 
 pub async fn run_commands() -> Result<()> {
     let current_directory = env::current_dir().context("problem fetching current directory")?;
+    let package_json_path = current_directory.join("package.json");
     let cli = Cli::parse();
 
     match &cli.subcommand {
         Subcommands::Init => {
             // init command throws an error if package.json file exist.
-            PackageJson::create()?;
+            PackageJson::init(&package_json_path)?;
         }
         Subcommands::Add(args) => {
             let mut registry_manager = RegistryManager::new(
                 current_directory.join("node_modules"),
                 current_directory.join(&args.virtual_store_dir),
+                package_json_path,
             )?;
             registry_manager.prepare()?;
             registry_manager.add_dependency(&args.package).await?;
