559f121b81df0cb134675e1138a877e20d78976d
Yagiz Nizipli <yagiz@nizipli.com>
2023-07-18 14:01:21 -0400

    chore: release v0.0.8

--- crates/cli/src/commands.rs
+++ crates/cli/src/commands.rs
@@ -5,7 +5,7 @@ use pacquet_package_json::DependencyGroup;
 #[derive(Parser, Debug)]
 #[command(name = "pacquet")]
 #[command(bin_name = "pacquet")]
-#[command(version = "0.0.1")]
+#[command(version = "0.0.8")]
 #[command(about = "Experimental package manager for node.js")]
 pub struct Cli {
     #[command(subcommand)]
