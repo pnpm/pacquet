c398c5d5e22ff35f051826693336017a70297cc9
Yagiz Nizipli <yagiz@nizipli.com>
2023-07-20 17:18:48 -0400

    feat: add integrity check while downloading

--- crates/tarball/src/lib.rs
+++ crates/tarball/src/lib.rs
@@ -1,14 +1,17 @@
+#![feature(error_generic_member_access, provide_any)]
+
 mod symlink;
 
 use std::{
+    backtrace::Backtrace,
     env,
-    fs::{self, File},
-    io::Write,
-    path::Path,
+    fs::{self},
+    io::Cursor,
+    path::PathBuf,
 };
 
-use flate2::read::GzDecoder;
-use futures_util::StreamExt;
+use libdeflater::DecompressionError;
+use ssri::{Algorithm, IntegrityOpts};
 use tar::Archive;
 use thiserror::Error;
 use tracing::{event, instrument, Level};
@@ -18,14 +21,32 @@ use crate::symlink::symlink_dir;
 
 #[derive(Error, Debug)]
 pub enum TarballError {
-    #[error("network error while downloading {0}")]
-    Network(#[from] reqwest::Error),
-    #[error("io error: {0}")]
-    Io(#[from] std::io::Error),
+    #[error("network error while downloading {source}")]
+    Network {
+        #[from]
+        source: reqwest::Error,
+        backtrace: Backtrace,
+    },
+    #[error("io error: {source}")]
+    Io {
+        #[from]
+        source: std::io::Error,
+        backtrace: Backtrace,
+    },
+    #[error("checksum mismatch. provided {provided} should match {expected}")]
+    ChecksumMismatch { provided: String, expected: String },
+    #[error("decompression error: {source}")]
+    Decompression {
+        #[from]
+        source: DecompressionError,
+        backtrace: Backtrace,
+    },
 }
 
 #[derive(Debug)]
-pub struct TarballManager;
+pub struct TarballManager {
+    http_client: reqwest::Client,
+}
 
 impl Default for TarballManager {
     fn default() -> Self {
@@ -35,32 +56,15 @@ impl Default for TarballManager {
 
 impl TarballManager {
     pub fn new() -> Self {
-        TarballManager {}
+        TarballManager { http_client: reqwest::Client::new() }
     }
 
     #[instrument]
-    async fn download(&self, url: &str, tarball_path: &Path) -> Result<(), TarballError> {
-        let mut stream = reqwest::get(url).await?.bytes_stream();
-        let mut file = File::create(tarball_path)?;
-        event!(Level::DEBUG, "downloading tarball to {}", tarball_path.display());
-
-        while let Some(item) = stream.next().await {
-            let chunk = item.map_err(TarballError::Network)?;
-            file.write_all(&chunk)?;
-        }
-
-        Ok(())
-    }
-
-    #[instrument]
-    fn extract(&self, tarball_path: &Path, extract_path: &Path) -> Result<(), TarballError> {
+    fn extract(&self, data: Vec<u8>, extract_path: &PathBuf) -> Result<(), TarballError> {
         let unpack_path = env::temp_dir().join(Uuid::new_v4().to_string());
         event!(Level::DEBUG, "unpacking tarball to {}", unpack_path.display());
-        let tar_gz = File::open(tarball_path)?;
-        let tar = GzDecoder::new(tar_gz);
-        let mut archive = Archive::new(tar);
+        let mut archive = Archive::new(Cursor::new(data));
         archive.unpack(&unpack_path)?;
-        fs::remove_file(tarball_path)?;
         fs::create_dir_all(extract_path)?;
         fs::rename(unpack_path.join("package"), extract_path)?;
         fs::remove_dir_all(&unpack_path)?;
@@ -68,37 +72,72 @@ impl TarballManager {
     }
 
     #[instrument]
+    fn verify_checksum(&self, data: &bytes::Bytes, integrity: &str) -> Result<(), TarballError> {
+        let expected = if integrity.starts_with("sha1-") {
+            let hash = IntegrityOpts::new().algorithm(Algorithm::Sha1).chain(data).result();
+            format!("sha1-{}", hash.to_hex().1)
+        } else {
+            IntegrityOpts::new().algorithm(Algorithm::Sha512).chain(data).result().to_string()
+        };
+
+        if integrity != expected {
+            Err(TarballError::ChecksumMismatch { provided: integrity.to_string(), expected })
+        } else {
+            Ok(())
+        }
+    }
+
+    #[instrument]
+    fn decompress_gzip(&self, gz_data: &[u8]) -> Result<Vec<u8>, TarballError> {
+        // gzip RFC1952: a valid gzip file has an ISIZE field in the
+        // footer, which is a little-endian u32 number representing the
+        // decompressed size. This is ideal for libdeflate, which needs
+        // preallocating the decompressed buffer.
+        let isize = {
+            let isize_start = gz_data.len() - 4;
+            let isize_bytes: [u8; 4] = gz_data[isize_start..].try_into().unwrap();
+            u32::from_le_bytes(isize_bytes) as usize
+        };
+
+        let mut decompressor = libdeflater::Decompressor::new();
+
+        let mut outbuf = vec![0; isize];
+        decompressor.gzip_decompress(gz_data, &mut outbuf)?;
+        Ok(outbuf)
+    }
+
+    #[instrument]
     pub async fn download_dependency(
         &self,
-        name: &str,
+        integrity: &str,
         url: &str,
-        save_path: &Path,
-        symlink_to: &Path,
+        save_path: &PathBuf,
+        symlink_to: &PathBuf,
     ) -> Result<(), TarballError> {
         let symlink_exists = symlink_to.is_symlink();
 
         // If name contains `/` such as @fastify/error, we need to make sure that @fastify folder
         // exists before we symlink to that directory.
-        if name.contains('/') && !symlink_exists {
-            fs::create_dir_all(symlink_to.parent().unwrap())?;
+        if let Some(parent_folder) = symlink_to.parent() {
+            fs::create_dir_all(parent_folder)?;
         }
 
         // Do not try to install dependency if this version already exists in package.json
         if save_path.exists() || symlink_exists {
             if !symlink_exists {
-                symlink_dir(&save_path.to_path_buf(), &symlink_to.to_path_buf())?;
+                symlink_dir(&save_path, &symlink_to)?;
             }
             return Ok(());
         }
 
-        let tarball_path = env::temp_dir().join(Uuid::new_v4().to_string());
-        self.download(url, &tarball_path).await?;
-
-        self.extract(&tarball_path, save_path)?;
+        let response = self.http_client.get(url).send().await?.bytes().await?;
+        self.verify_checksum(&response, integrity)?;
+        let data = self.decompress_gzip(&response)?;
+        self.extract(data, save_path)?;
 
         // TODO: Currently symlink paths are absolute paths.
         // If you move the root folder to a different path, all symlinks will be broken.
-        symlink_dir(&save_path.to_path_buf(), &symlink_to.to_path_buf())?;
+        symlink_dir(&save_path, &symlink_to)?;
 
         Ok(())
     }
@@ -119,6 +158,7 @@ mod tests {
         let parent_folder = env::temp_dir().join(id.to_string());
         fs::create_dir_all(parent_folder.join("store")).expect("failed to create folder");
         fs::create_dir_all(parent_folder.join("node_modules")).expect("failed to create folder");
+        env::set_current_dir(&parent_folder).unwrap();
         parent_folder
     }
 
@@ -136,6 +176,7 @@ mod tests {
 
     #[tokio::test]
     async fn packages_under_orgs_should_work() {
+        let current_path = env::current_dir().unwrap();
         let parent_folder = create_folders();
         let store_path = parent_folder.join("store");
         let node_modules_path = parent_folder.join("node_modules");
@@ -146,10 +187,10 @@ mod tests {
 
         manager
             .download_dependency(
-                "@fastify/error",
+                "sha512-dj7vjIn1Ar8sVXj2yAXiMNCJDmS9MQ9XMlIecX2dIzzhjSHCyKo4DdXjXMs7wKW2kj6yvVRSpuQjOZ3YLrh56w==",
                 "https://registry.npmjs.org/@fastify/error/-/error-3.3.0.tgz",
-                &save_path.to_path_buf(),
-                &symlink_path.to_path_buf(),
+                &save_path,
+                &symlink_path,
             )
             .await
             .unwrap();
@@ -157,13 +198,19 @@ mod tests {
         // Validate if we delete the tar.gz file
         assert!(!store_path.join("@fastify+error@3.3.0.tar.gz").exists());
         // Make sure we create store path with normalized name
-        assert!(store_path.join("@fastify+error@3.3.0").is_dir());
+        assert!(save_path.as_path().is_dir());
+        assert!(save_path.join("package.json").is_file());
         // Make sure we create a symlink on node_modules folder
         assert!(symlink_path.exists());
         assert!(symlink_path.is_symlink());
+        // Make sure the symlink is looking to the correct place
+        assert_eq!(fs::read_link(&symlink_path).unwrap(), save_path);
         //Make sure we create a @fastify folder inside node_modules
         assert!(node_modules_path.join("@fastify").is_dir());
+        assert!(node_modules_path.join("@fastify/error").is_symlink());
+        assert!(node_modules_path.join("@fastify/error/package.json").is_file());
 
+        env::set_current_dir(current_path).unwrap();
         fs::remove_dir_all(&parent_folder).unwrap();
     }
 }
--- crates/tarball/src/symlink.rs
+++ crates/tarball/src/symlink.rs
@@ -1,11 +1,11 @@
-use std::{io, os, path::PathBuf};
+use std::{io, os, path::Path};
 
 #[cfg(unix)]
-pub fn symlink_dir(original: &PathBuf, link: &PathBuf) -> io::Result<()> {
+pub fn symlink_dir<P: AsRef<Path>>(original: P, link: P) -> io::Result<()> {
     os::unix::fs::symlink(original, link)
 }
 
 #[cfg(windows)]
-pub fn symlink_dir(original: &PathBuf, link: &PathBuf) -> io::Result<()> {
+pub fn symlink_dir<P: AsRef<Path>>(original: P, link: P) -> io::Result<()> {
     os::windows::fs::symlink_dir(original, link)
 }
