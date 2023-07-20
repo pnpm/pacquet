caf602c044accc3751dd30946c19efcc5da7993c
Yagiz Nizipli <yagiz@nizipli.com>
2023-07-19 21:27:13 -0400

    fix: move npmrc to registry

--- crates/cli/src/lib.rs
+++ crates/cli/src/lib.rs
@@ -6,7 +6,6 @@ use std::env;
 use anyhow::{Context, Result};
 use clap::Parser;
 use commands::{Cli, Subcommands};
-use pacquet_npmrc::get_current_npmrc;
 use pacquet_package_json::PackageJson;
 use pacquet_registry::RegistryManager;
 
@@ -16,7 +15,6 @@ pub async fn run_commands() -> Result<()> {
     enable_tracing_by_env();
     let current_directory = env::current_dir().context("problem fetching current directory")?;
     let package_json_path = current_directory.join("package.json");
-    let config = get_current_npmrc();
     let cli = Cli::parse();
 
     match &cli.subcommand {
@@ -25,11 +23,7 @@ pub async fn run_commands() -> Result<()> {
             PackageJson::init(&package_json_path)?;
         }
         Subcommands::Add(args) => {
-            let mut registry_manager = RegistryManager::new(
-                config.modules_dir,
-                config.virtual_store_dir,
-                package_json_path,
-            )?;
+            let mut registry_manager = RegistryManager::new(package_json_path)?;
             // TODO if a package already exists in another dependency group, we don't remove
             // the existing entry.
             registry_manager.add_dependency(&args.package, args.get_dependency_group()).await?;
