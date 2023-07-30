0691789c1bcc415e8756ff88596a20eb00be458d
Yagiz Nizipli <yagiz@nizipli.com>
2023-07-30 17:14:21 -0400

    perf: update benchmark to remove from store

--- benchmark/add.sh
+++ benchmark/add.sh
@@ -9,9 +9,10 @@ YARN="yarn add fastify --silent"
 BUN="bun add fastify --no-cache --silent --backend=symlink"
 
 FILE_CLEAN="rm -rf package.json node_modules .yarn yarn.lock .pnp* && echo {} > package.json || true"
+PACQUET_CLEAN="../target/release/pacquet store prune"
 PNPM_CLEAN="pnpm store prune"
 YARN_CLEAN="yarn cache clean --all"
-CLEANUP="${PNPM_CLEAN} && ${YARN_CLEAN} && ${FILE_CLEAN}"
+CLEANUP="${PACQUET_CLEAN} && ${PNPM_CLEAN} && ${YARN_CLEAN} && ${FILE_CLEAN}"
 
 $FILE_CLEAN
 
