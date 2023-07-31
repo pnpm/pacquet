50b467cba29d9e58ba36a689434001a2a8d317ec
Yakiyo <yakiyo1206@gmail.com>
2023-07-31 04:28:02 +0000

    feat: append additional args to script invocation, if any

--- crates/cli/src/commands.rs
+++ crates/cli/src/commands.rs
@@ -87,6 +87,10 @@ impl AddArgs {
 pub struct RunArgs {
     /// A pre-defined package script.
     pub command: String,
+
+    /// Any additional arguments passed after the script name
+    pub args: Vec<String>,
+
     /// You can use the --if-present flag to avoid exiting with a non-zero exit code when the
     /// script is undefined. This lets you run potentially undefined scripts without breaking the
     /// execution chain.
