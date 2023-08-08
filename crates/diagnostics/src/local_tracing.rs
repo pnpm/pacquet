02d8e9a0401d92c75db35471b8e0cc9601ce585d
khai96_ <hvksmr1996@gmail.com>
2023-08-08 12:52:57 +0700

    refactor: one less `.expect`, one less panic

--- crates/diagnostics/src/local_tracing.rs
+++ crates/diagnostics/src/local_tracing.rs
@@ -6,12 +6,13 @@ use tracing_subscriber::{fmt::format::FmtSpan, EnvFilter, Layer};
 static IS_TRACING_ENABLED: AtomicBool = AtomicBool::new(false);
 
 pub fn enable_tracing_by_env() {
-    let trace_var = std::env::var("TRACE").ok();
-    let is_enable_tracing = trace_var.is_some();
+    let Ok(trace_var) = std::env::var("TRACE") else {
+        return
+    };
 
-    if is_enable_tracing && !IS_TRACING_ENABLED.swap(true, std::sync::atomic::Ordering::SeqCst) {
+    if !IS_TRACING_ENABLED.swap(true, std::sync::atomic::Ordering::SeqCst) {
         use tracing_subscriber::{fmt, prelude::*};
-        let layers = generate_common_layers(trace_var);
+        let layers = generate_common_layers(&trace_var);
 
         tracing_subscriber::registry()
             .with(layers)
@@ -22,24 +23,21 @@ pub fn enable_tracing_by_env() {
 }
 
 fn generate_common_layers(
-    trace_var: Option<String>,
+    trace_var: &str,
 ) -> Vec<Box<dyn Layer<tracing_subscriber::Registry> + Send + Sync>> {
-    let default_level = trace_var.as_ref().and_then(|var| Level::from_str(var).ok());
-
     let mut layers = vec![];
-    if let Some(default_level) = default_level {
+    if let Ok(default_level) = Level::from_str(trace_var) {
         layers.push(
             tracing_subscriber::filter::Targets::new()
                 .with_targets(vec![("pacquet_tarball", default_level)])
                 .boxed(),
         );
     } else {
-        // SAFETY: we know that trace_var is `Ok(String)` now,
-        // for the second unwrap, if we can't parse the directive, then the tracing result would be
+        // SAFETY: for the `expect`, if we can't parse the directive, then the tracing result would be
         // unexpected, then panic is reasonable
         let env_layer = EnvFilter::builder()
             .with_regex(true)
-            .parse(trace_var.expect("Should not be empty"))
+            .parse(trace_var)
             .expect("Parse tracing directive syntax failed,for details about the directive syntax you could refer https://docs.rs/tracing-subscriber/latest/tracing_subscriber/filter/struct.EnvFilter.html#directives");
 
         layers.push(env_layer.boxed());
