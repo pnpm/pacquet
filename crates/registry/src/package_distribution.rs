6bd33e9e380a1486bd2206f1062853f241e41342
khai96_ <hvksmr1996@gmail.com>
2023-08-16 13:58:50 +0700

    docs: add a TODO

--- crates/registry/src/package_distribution.rs
+++ crates/registry/src/package_distribution.rs
@@ -3,7 +3,7 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Debug, Default, Clone, Eq)]
 #[serde(rename_all = "camelCase")]
 pub struct PackageDistribution {
-    pub integrity: String,
+    pub integrity: String, // TODO: why fetching typescript cause error here?
     pub shasum: String,
     pub tarball: String,
     pub file_count: Option<usize>,
