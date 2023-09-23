9411705adc44ee48cccbdd9877b511c69ae518a4
khai96_ <hvksmr1996@gmail.com>
2023-09-23 19:46:08 +0700

    docs: reference the typescript interface

--- crates/lockfile/src/lib.rs
+++ crates/lockfile/src/lib.rs
@@ -25,7 +25,8 @@ pub struct LockfileSettings {
     exclude_links_from_lockfile: bool,
 }
 
-/// Specification: https://github.com/pnpm/spec/blob/master/lockfile/6.0.md
+/// * Specification: https://github.com/pnpm/spec/blob/master/lockfile/6.0.md
+/// * Reference: https://github.com/pnpm/pnpm/blob/main/lockfile/lockfile-types/src/index.ts
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Lockfile {
