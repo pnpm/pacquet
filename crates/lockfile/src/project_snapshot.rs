4e2a1a2f9c00217bbd8c0296f3bf9ad5b8034663
khai96_ <hvksmr1996@gmail.com>
2023-09-25 00:21:12 +0700

    feat: deny_unknown_fields for MultiProjectSnapshot

--- crates/lockfile/src/project_snapshot.rs
+++ crates/lockfile/src/project_snapshot.rs
@@ -23,7 +23,7 @@ pub struct ProjectSnapshot {
 
 /// Snapshot of a multi-project monorepo.
 #[derive(Debug, Default, PartialEq, Deserialize, Serialize)]
-#[serde(rename_all = "camelCase")]
+#[serde(deny_unknown_fields, rename_all = "camelCase")]
 pub struct MultiProjectSnapshot {
     pub importers: HashMap<String, ProjectSnapshot>,
 }
