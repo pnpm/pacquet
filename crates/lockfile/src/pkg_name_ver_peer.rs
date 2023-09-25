63eefdb76b221c8e3df787f83581ab3c97bb0221
khai96_ <hvksmr1996@gmail.com>
2023-09-26 01:59:31 +0700

    test: PkgNameVerPeer

--- crates/lockfile/src/pkg_name_ver_peer.rs
+++ crates/lockfile/src/pkg_name_ver_peer.rs
@@ -27,3 +27,43 @@ pub type PkgNameVerPeer = PkgNameSuffix<PkgVerPeer>;
 
 /// Error when parsing [`PkgVerPeer`] from a string.
 pub type ParsePkgNameVerPeerError = ParsePkgNameSuffixError<Infallible>;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pipe_trait::Pipe;
+    use pretty_assertions::assert_eq;
+
+    fn name_peer_ver(name: &str, peer_ver: &str) -> PkgNameVerPeer {
+        let peer_ver = peer_ver.to_string().pipe(PkgVerPeer);
+        PkgNameVerPeer::new(name, peer_ver)
+    }
+
+    #[test]
+    fn parse() {
+        macro_rules! case {
+            ($input:expr => $output:expr) => {{
+                let input = $input;
+                eprintln!("CASE: {input:?}");
+                let received: PkgNameVerPeer = input.parse().unwrap();
+                let expected = $output;
+                assert_eq!(&received, &expected);
+            }};
+        }
+
+        case!(
+            "react-json-view@1.21.3(@types/react@17.0.49)(react-dom@17.0.2)(react@17.0.2)" => name_peer_ver(
+                "react-json-view",
+                "1.21.3(@types/react@17.0.49)(react-dom@17.0.2)(react@17.0.2)",
+            )
+        );
+        case!("react-json-view@1.21.3" => name_peer_ver("react-json-view", "1.21.3"));
+        case!(
+            "@algolia/autocomplete-core@1.9.3(@algolia/client-search@4.18.0)(algoliasearch@4.18.0)(search-insights@2.6.0)" => name_peer_ver(
+                "@algolia/autocomplete-core",
+                "1.9.3(@algolia/client-search@4.18.0)(algoliasearch@4.18.0)(search-insights@2.6.0)",
+            )
+        );
+        case!("@algolia/autocomplete-core@1.9.3" => name_peer_ver("@algolia/autocomplete-core", "1.9.3"));
+    }
+}
