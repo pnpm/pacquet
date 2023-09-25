bf3716b4de49441a45f5c3043a438ee97530cd20
khai96_ <hvksmr1996@gmail.com>
2023-09-25 16:38:10 +0700

    refactor: consistency

--- crates/lockfile/src/dependency.rs
+++ crates/lockfile/src/dependency.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, PartialEq, Deserialize, Serialize)]
+#[serde(deny_unknown_fields, rename_all = "camelCase")]
 pub struct LockfileDependency {
     specifier: String,
     version: String, // TODO: LockfileDependencyVersion syntax: 10.9.1(@types/node@18.7.19)(typescript@5.1.6)
