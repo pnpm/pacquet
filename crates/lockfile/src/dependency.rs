dc5a03df1eb7a802877efaf990036babc5f65a56
khai96_ <hvksmr1996@gmail.com>
2023-09-26 14:00:18 +0700

    feat: change type of LockfileDependency::version

--- crates/lockfile/src/dependency.rs
+++ crates/lockfile/src/dependency.rs
@@ -1,8 +1,9 @@
+use crate::PkgVerPeer;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, PartialEq, Deserialize, Serialize)]
 #[serde(deny_unknown_fields, rename_all = "camelCase")]
 pub struct LockfileDependency {
     specifier: String,
-    version: String, // TODO: LockfileDependencyVersion syntax: 10.9.1(@types/node@18.7.19)(typescript@5.1.6)
+    version: PkgVerPeer,
 }
