ba3d2e266976e915f98233cfe0892a2cd275b24f
khai96_ <hvksmr1996@gmail.com>
2023-10-03 12:48:14 +0700

    feat(lockfile): skip serializing more `None`s

--- crates/lockfile/src/package_snapshot.rs
+++ crates/lockfile/src/package_snapshot.rs
@@ -15,7 +15,9 @@ pub struct PackageSnapshot {
     #[serde(skip_serializing_if = "Option::is_none")]
     pub id: Option<String>,
 
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub name: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub version: Option<String>,
 
     #[serde(skip_serializing_if = "Option::is_none")]
