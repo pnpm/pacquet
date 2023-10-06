1cca0c59b55ae74b1c42c54a7bf9d304e6dd03ab
khai96_ <hvksmr1996@gmail.com>
2023-10-06 22:37:56 +0700

    refactor: rename a mod

--- crates/package_manager/src/lib.rs
+++ crates/package_manager/src/lib.rs
@@ -1,7 +1,7 @@
-mod auto_import;
+mod import_pkg;
 mod symlink_pkg;
 mod virtual_dir;
 
-pub use auto_import::{auto_import, AutoImportError};
+pub use import_pkg::{auto_import, AutoImportError};
 pub use symlink_pkg::symlink_pkg;
 pub use virtual_dir::{create_virtdir_by_snapshot, CreateVirtdirError};
