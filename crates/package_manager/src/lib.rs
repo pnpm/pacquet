ae3c114ae9b22acd9242853d264080ee67995699
khai96_ <hvksmr1996@gmail.com>
2023-10-12 23:14:29 +0700

    refactor: mod should have same name

--- crates/package_manager/src/lib.rs
+++ crates/package_manager/src/lib.rs
@@ -1,19 +1,19 @@
-mod cas;
+mod create_cas_files;
+mod create_symlink_layout;
+mod create_virtual_dir_by_snapshot;
+mod create_virtual_store;
 mod install_package_by_snapshot;
 mod install_package_from_registry;
 mod link_file;
-mod symlink_layout;
 mod symlink_package;
-mod virtual_dir;
-mod virtual_store;
 
-pub use cas::{create_cas_files, CreateCasFilesError};
+pub use create_cas_files::{create_cas_files, CreateCasFilesError};
+pub use create_symlink_layout::create_symlink_layout;
+pub use create_virtual_dir_by_snapshot::{CreateVirtualDirBySnapshot, CreateVirtualDirError};
+pub use create_virtual_store::CreateVirtualStore;
 pub use install_package_by_snapshot::{InstallPackageBySnapshot, InstallPackageBySnapshotError};
 pub use install_package_from_registry::{
     InstallPackageFromRegistry, InstallPackageFromRegistryError,
 };
 pub use link_file::{link_file, LinkFileError};
-pub use symlink_layout::create_symlink_layout;
 pub use symlink_package::{symlink_package, SymlinkPackageError};
-pub use virtual_dir::{CreateVirtualDirBySnapshot, CreateVirtualDirError};
-pub use virtual_store::CreateVirtualStore;
