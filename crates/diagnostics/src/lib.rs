58d12f937a72a74a88b249cc55918feb0f29ab06
khai96_ <hvksmr1996@gmail.com>
2023-10-14 18:19:21 +0700

    refactor: remove unused type aliases

--- crates/diagnostics/src/lib.rs
+++ crates/diagnostics/src/lib.rs
@@ -5,8 +5,3 @@ pub use thiserror;
 pub use tracing;
 
 pub use local_tracing::enable_tracing_by_env;
-
-pub type Error = miette::Error;
-pub type Severity = miette::Severity;
-pub type Report = miette::Report;
-pub type Result<T> = miette::Result<T>;
