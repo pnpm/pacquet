35b6db484c77d33f75b17dc765529b2d5d75f8df
khai96_ <hvksmr1996@gmail.com>
2023-10-14 18:27:08 +0700

    feat(executor): distinct error variants

--- crates/executor/src/lib.rs
+++ crates/executor/src/lib.rs
@@ -1,18 +1,24 @@
-use derive_more::{Display, Error, From};
+use derive_more::{Display, Error};
 use miette::Diagnostic;
 use std::process::Command;
 
-#[derive(Debug, Display, Error, Diagnostic, From)]
+#[derive(Debug, Display, Error, Diagnostic)]
 #[non_exhaustive]
 pub enum ExecutorError {
-    #[diagnostic(code(pacquet_executor::io_error))]
-    Io(#[error(source)] std::io::Error),
+    #[display(fmt = "Failed to spawn command: {_0}")]
+    #[diagnostic(code(pacquet_executor::spawn_command))]
+    SpawnCommand(#[error(source)] std::io::Error),
+
+    #[display(fmt = "Process exits with an error: {_0}")]
+    #[diagnostic(code(pacquet_executor::wait_process))]
+    WaitProcess(#[error(source)] std::io::Error),
 }
 
 pub fn execute_shell(command: &str) -> Result<(), ExecutorError> {
-    let mut cmd = Command::new("sh").arg("-c").arg(command).spawn()?;
+    let mut cmd =
+        Command::new("sh").arg("-c").arg(command).spawn().map_err(ExecutorError::SpawnCommand)?;
 
-    cmd.wait()?;
+    cmd.wait().map_err(ExecutorError::WaitProcess)?;
 
     Ok(())
 }
