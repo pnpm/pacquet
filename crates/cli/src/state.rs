d5dc1660e319a2c586416987749b60560d604b00
khai96_ <hvksmr1996@gmail.com>
2023-10-15 18:48:58 +0700

    docs: fields of State

--- crates/cli/src/state.rs
+++ crates/cli/src/state.rs
@@ -10,10 +10,15 @@ use std::path::PathBuf;
 
 /// Application state when running `pacquet run` or `pacquet install`.
 pub struct State {
+    /// Configuration read from `.npmrc`
     pub config: &'static Npmrc,
+    /// Data from the `package.json` file.
     pub package_json: PackageJson,
+    /// Data from the `pnpm-lock.yaml` file.
     pub lockfile: Option<Lockfile>,
+    /// HTTP client to make HTTP requests.
     pub http_client: Client,
+    /// Shared cache that store downloaded tarballs.
     pub tarball_cache: Cache,
 }
 
