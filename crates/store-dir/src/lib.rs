54a81fd6310699aacb5ca911dad957790e0c87bd
khai96_ <hvksmr1996@gmail.com>
2023-10-30 00:15:32 +0700

    feat: pacquet-store-dir

---
+++ crates/store-dir/src/lib.rs
@@ -0,0 +1,35 @@
+use std::path::PathBuf;
+
+/// Represent a store directory.
+///
+/// * The store directory stores all files that were acquired by installing packages with pacquet or pnpm.
+/// * The files in `node_modules` directories are hardlinks or reflinks to the files in the store directory.
+/// * The store directory can and often act as a global shared cache of all installation of different workspaces.
+/// * The location of the store directory can be customized by `store-dir` field.
+pub struct StoreDir {
+    /// Path to the root of the store directory from which all sub-paths are derived.
+    ///
+    /// Consumer of this struct should interact with the sub-paths instead of this path.
+    root: PathBuf,
+}
+
+impl StoreDir {
+    /// Get `{store}/v3`.
+    fn v3(&self) -> PathBuf {
+        self.root.join("v3")
+    }
+
+    /// The directory that contains all files from the once-installed packages.
+    fn files(&self) -> PathBuf {
+        self.v3().join("files")
+    }
+
+    /// Path to a file in the store directory.
+    ///
+    /// **Parameters:**
+    /// * `head` is the first 2 hexadecimal digit of the file address.
+    /// * `tail` is the rest of the address and an optional suffix.
+    pub fn file_path_by_hash_str(&self, head: &str, tail: &str) -> PathBuf {
+        self.files().join(format!("{head}{tail}"))
+    }
+}
