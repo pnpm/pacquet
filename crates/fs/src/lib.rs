c7e8d9a1208a6022120e1ab66a9552821d44b152
khai96_ <hvksmr1996@gmail.com>
2023-10-31 20:24:49 +0700

    refactor: move a function to pacquet-fs

--- crates/fs/src/lib.rs
+++ crates/fs/src/lib.rs
@@ -1,4 +1,4 @@
-use std::{io, path::Path};
+use std::{fs, io, path::Path};
 
 /// Create a symlink to a directory.
 ///
@@ -8,4 +8,19 @@ pub fn symlink_dir(original: &Path, link: &Path) -> io::Result<()> {
     return std::os::unix::fs::symlink(original, link);
     #[cfg(windows)]
     return junction::create(original, link); // junctions instead of symlinks because symlinks may require elevated privileges.
+}
+
+/// Write `content` to `file_path` unless it already exists.
+///
+/// Ancestor directories will be created if they don't already exist.
+///
+/// **TODO:** separate 2 error cases and add more details
+pub fn ensure_file(file_path: &Path, content: &[u8]) -> io::Result<()> {
+    if file_path.exists() {
+        return Ok(());
+    }
+
+    let parent_dir = file_path.parent().unwrap();
+    fs::create_dir_all(parent_dir)?;
+    fs::write(file_path, content)
 }
--- crates/store-dir/src/write_file.rs
+++ crates/store-dir/src/write_file.rs
@@ -1,23 +1,10 @@
 use crate::{FileHash, FileSuffix, StoreDir, TarballIndex};
 use derive_more::{Display, Error};
 use miette::Diagnostic;
+use pacquet_fs::ensure_file;
 use sha2::{Digest, Sha512};
 use ssri::Integrity;
-use std::{
-    fs, io,
-    path::{Path, PathBuf},
-};
-
-// TODO: separate 2 error cases and add more details
-fn write_file_if_not_exist(file_path: &Path, content: &[u8]) -> io::Result<()> {
-    if file_path.exists() {
-        return Ok(());
-    }
-
-    let parent_dir = file_path.parent().unwrap();
-    fs::create_dir_all(parent_dir)?;
-    fs::write(file_path, content)
-}
+use std::{fs, io, path::PathBuf};
 
 /// Error type of [`StoreDir::write_non_index_file`].
 #[derive(Debug, Display, Error, Diagnostic)]
@@ -35,7 +22,7 @@ impl StoreDir {
         let file_hash = Sha512::digest(buffer);
         let file_path = self.file_path_by_content_address(file_hash, suffix);
 
-        write_file_if_not_exist(&file_path, buffer).map_err(WriteNonIndexFileError::WriteFile)?;
+        ensure_file(&file_path, buffer).map_err(WriteNonIndexFileError::WriteFile)?;
 
         #[cfg(unix)]
         {
@@ -66,7 +53,7 @@ impl StoreDir {
         let file_path = self.tarball_index_file_path(tarball_integrity);
         let index_content =
             serde_json::to_string(&index_content).expect("convert a TarballIndex to JSON");
-        write_file_if_not_exist(&file_path, index_content.as_bytes())
+        ensure_file(&file_path, index_content.as_bytes())
             .map_err(WriteTarballIndexFileError::WriteFile)
     }
 }
