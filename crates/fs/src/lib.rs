3ba598679c784b2a961363a12193f09a092581b8
khai96_ <hvksmr1996@gmail.com>
2023-10-31 21:03:14 +0700

    feat(fs): make_file_executable

--- crates/fs/src/lib.rs
+++ crates/fs/src/lib.rs
@@ -1,4 +1,4 @@
-use std::{io, path::Path};
+use std::{fs, io, path::Path};
 
 /// Create a symlink to a directory.
 ///
@@ -8,4 +8,18 @@ pub fn symlink_dir(original: &Path, link: &Path) -> io::Result<()> {
     return std::os::unix::fs::symlink(original, link);
     #[cfg(windows)]
     return junction::create(original, link); // junctions instead of symlinks because symlinks may require elevated privileges.
+}
+
+/// Set file mode to 777 on POSIX platforms such as Linux or macOS,
+/// or do nothing on Windows.
+pub fn make_file_executable(file_path: &Path) -> io::Result<()> {
+    #[cfg(unix)]
+    return {
+        use std::{fs::Permissions, os::unix::fs::PermissionsExt};
+        let permissions = Permissions::from_mode(0o777);
+        fs::set_permissions(file_path, permissions)
+    };
+
+    #[cfg(windows)]
+    return Ok(());
 }
