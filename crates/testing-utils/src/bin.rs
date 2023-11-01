14a312589476bf380537a3157d57a48d66e5d37b
khai96_ <hvksmr1996@gmail.com>
2023-11-02 00:41:58 +0700

    refactor: extract default npmrc

--- crates/testing-utils/src/bin.rs
+++ crates/testing-utils/src/bin.rs
@@ -4,19 +4,17 @@ use std::{fs, path::PathBuf, process::Command};
 use tempfile::{tempdir, TempDir};
 use text_block_macros::text_block_fnl;
 
+const DEFAULT_NPMRC: &str = text_block_fnl! {
+    "store-dir=../pacquet-store"
+    "cache-dir=../pacquet-cache"
+};
+
 pub fn pacquet_with_temp_cwd(create_npmrc: bool) -> (Command, TempDir, PathBuf) {
     let root = tempdir().expect("create temporary directory");
     let workspace = root.path().join("workspace");
     fs::create_dir(&workspace).expect("create temporary workspace for pacquet");
     if create_npmrc {
-        fs::write(
-            workspace.join(".npmrc"),
-            text_block_fnl! {
-                "store-dir=../pacquet-store"
-                "cache-dir=../pacquet-cache"
-            },
-        )
-        .expect("write to .npmrc");
+        fs::write(workspace.join(".npmrc"), DEFAULT_NPMRC).expect("write to .npmrc");
     }
     let command = Command::cargo_bin("pacquet")
         .expect("find the pacquet binary")
