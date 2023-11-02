fa24b852b42cb10f2050e2407b70d84f4ad4d129
khai96_ <hvksmr1996@gmail.com>
2023-11-02 16:47:04 +0700

    refactor: convert get_all_files to chain

--- crates/testing-utils/src/fs.rs
+++ crates/testing-utils/src/fs.rs
@@ -31,31 +31,14 @@ pub fn get_all_folders(root: &Path) -> Vec<String> {
 }
 
 pub fn get_all_files(root: &Path) -> Vec<String> {
-    let mut files = Vec::new();
-    for entry in WalkDir::new(root) {
-        let entry = entry.unwrap();
-        let entry_path = entry.path();
-
-        if entry.file_type().is_dir() {
-            continue;
-        }
-
-        // We need this mutation to ensure that both Unix and Windows paths resolves the same.
-        // TODO: Find a better way to do this?
-        let simple_path = entry_path
-            .strip_prefix(root)
-            .unwrap()
-            .components()
-            .map(|c| c.as_os_str().to_str().expect("invalid UTF-8"))
-            .collect::<Vec<_>>()
-            .join("/");
-
-        if !simple_path.is_empty() {
-            files.push(simple_path);
-        }
-    }
-    files.sort();
-    files
+    WalkDir::new(root)
+        .sort_by_file_name()
+        .into_iter()
+        .map(|entry| entry.expect("access entry"))
+        .filter(|entry| !entry.file_type().is_dir())
+        .map(|entry| normalized_suffix(entry.path(), root))
+        .filter(|suffix| !suffix.is_empty())
+        .collect()
 }
 
 // Helper function to check if a path is a symlink or junction
