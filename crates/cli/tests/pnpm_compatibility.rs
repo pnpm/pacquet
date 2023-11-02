3c505bae5e99ff1f8edab6d0e3cd2f4a776b479d
khai96_ <hvksmr1996@gmail.com>
2023-11-02 11:33:42 +0700

    test(cli/install): same index files as pnpm

--- crates/cli/tests/pnpm_compatibility.rs
+++ crates/cli/tests/pnpm_compatibility.rs
@@ -1,7 +1,11 @@
 #![cfg(unix)] // running this on windows result in 'program not found'
+pub mod _utils;
+pub use _utils::*;
+
 use assert_cmd::prelude::*;
 use command_extra::CommandExtra;
 use pacquet_testing_utils::{bin::pacquet_and_pnpm_with_temp_cwd, fs::get_all_files};
+use pipe_trait::Pipe;
 use pretty_assertions::assert_eq;
 use std::fs;
 
@@ -70,6 +74,54 @@ fn same_file_structure() {
 
     eprintln!("Produce the same store dir structure");
     assert_eq!(&pacquet_store_files, &pnpm_store_files);
+
+    drop(root); // cleanup
+}
+
+#[test]
+fn same_index_file_contents() {
+    let (pacquet, pnpm, root, workspace) = pacquet_and_pnpm_with_temp_cwd(true);
+
+    let store_dir = root.path().join("pacquet-store");
+    let modules_dir = workspace.join("node_modules");
+    let cleanup = || {
+        eprintln!("Cleaning up...");
+        fs::remove_dir_all(&store_dir).expect("delete store dir");
+        fs::remove_dir_all(&modules_dir).expect("delete node_modules");
+    };
+
+    eprintln!("Creating package.json...");
+    let manifest_path = workspace.join("package.json");
+    let package_json_content = serde_json::json!({
+        "dependencies": {
+            "is-odd": "3.0.1",
+        },
+        "devDependencies": {
+            "fast-decode-uri-component": "1.0.1",
+        },
+    });
+    fs::write(manifest_path, package_json_content.to_string()).expect("write to package.json");
+
+    eprintln!("Installing with pacquet...");
+    pacquet.with_arg("install").assert().success();
+    let pacquet_index_file_contents = store_dir
+        .pipe_as_ref(index_file_contents)
+        .pipe(serde_json::to_value)
+        .expect("serialize pacquet index file contents");
+
+    cleanup();
+
+    eprintln!("Installing with pnpm...");
+    pnpm.with_args(["install", "--ignore-scripts"]).assert().success();
+    let pnpm_index_file_contents = store_dir
+        .pipe_as_ref(index_file_contents)
+        .pipe(serde_json::to_value)
+        .expect("serialize pnpm index file contents");
+
+    cleanup();
+
+    eprintln!("Produce the same store dir structure");
+    assert_eq!(&pacquet_index_file_contents, &pnpm_index_file_contents);
 
     drop(root); // cleanup
 }
