433a344bccdff778ac6ef380f8b2ee3ef7c8c1f0
khai96_ <hvksmr1996@gmail.com>
2023-11-16 02:18:01 +0700

    test(cli/install): use registry-mock

--- crates/cli/tests/install.rs
+++ crates/cli/tests/install.rs
@@ -4,7 +4,7 @@ pub use _utils::*;
 use assert_cmd::prelude::*;
 use command_extra::CommandExtra;
 use pacquet_testing_utils::{
-    bin::{AddDefaultNpmrcInfo, CommandTempCwd},
+    bin::{AddMockedRegistry, CommandTempCwd},
     fs::{get_all_files, get_all_folders, is_symlink_or_junction},
 };
 use pipe_trait::Pipe;
@@ -13,17 +13,14 @@ use std::fs;
 #[test]
 fn should_install_dependencies() {
     let CommandTempCwd { pacquet, root, workspace, npmrc_info, .. } =
-        CommandTempCwd::init().add_default_npmrc();
-    let AddDefaultNpmrcInfo { store_dir, .. } = npmrc_info;
+        CommandTempCwd::init().add_mocked_registry();
+    let AddMockedRegistry { store_dir, mock_instance, .. } = npmrc_info;
 
     eprintln!("Creating package.json...");
     let manifest_path = workspace.join("package.json");
     let package_json_content = serde_json::json!({
         "dependencies": {
-            "is-odd": "3.0.1",
-        },
-        "devDependencies": {
-            "fast-decode-uri-component": "1.0.1",
+            "@pnpm.e2e/hello-world-js-bin-parent": "1.0.0",
         },
     });
     fs::write(&manifest_path, package_json_content.to_string()).expect("write to package.json");
@@ -32,38 +29,35 @@ fn should_install_dependencies() {
     pacquet.with_arg("install").assert().success();
 
     eprintln!("Make sure the package is installed");
-    assert!(is_symlink_or_junction(&workspace.join("node_modules/is-odd")).unwrap());
-    assert!(workspace.join("node_modules/.pnpm/is-odd@3.0.1").exists());
+    let symlink_path = workspace.join("node_modules/@pnpm.e2e/hello-world-js-bin-parent");
+    assert!(is_symlink_or_junction(&symlink_path).unwrap());
+    let virtual_path =
+        workspace.join("node_modules/.pnpm/@pnpm.e2e+hello-world-js-bin-parent@1.0.0");
+    assert!(virtual_path.exists());
 
     eprintln!("Make sure it installs direct dependencies");
-    assert!(!workspace.join("node_modules/is-number").exists());
-    assert!(workspace.join("node_modules/.pnpm/is-number@6.0.0").exists());
-
-    eprintln!("Make sure we install dev-dependencies as well");
-    assert!(
-        is_symlink_or_junction(&workspace.join("node_modules/fast-decode-uri-component")).unwrap()
-    );
-    assert!(workspace.join("node_modules/.pnpm/fast-decode-uri-component@1.0.1").is_dir());
+    assert!(!workspace.join("node_modules/@pnpm.e2e/hello-world-js-bin").exists());
+    assert!(workspace.join("node_modules/.pnpm/@pnpm.e2e+hello-world-js-bin@1.0.0").exists());
 
     eprintln!("Snapshot");
     let workspace_folders = get_all_folders(&workspace);
     let store_files = get_all_files(&store_dir);
     insta::assert_debug_snapshot!((workspace_folders, store_files));
 
-    drop(root); // cleanup
+    drop((root, mock_instance)); // cleanup
 }
 
 #[test]
 fn should_install_exec_files() {
     let CommandTempCwd { pacquet, root, workspace, npmrc_info, .. } =
-        CommandTempCwd::init().add_default_npmrc();
-    let AddDefaultNpmrcInfo { store_dir, .. } = npmrc_info;
+        CommandTempCwd::init().add_mocked_registry();
+    let AddMockedRegistry { store_dir, mock_instance, .. } = npmrc_info;
 
     eprintln!("Creating package.json...");
     let manifest_path = workspace.join("package.json");
     let package_json_content = serde_json::json!({
         "dependencies": {
-            "pretty-exec": "0.3.10",
+            "@pnpm.e2e/hello-world-js-bin-parent": "1.0.0",
         },
     });
     fs::write(&manifest_path, package_json_content.to_string()).expect("write to package.json");
@@ -110,23 +104,20 @@ fn should_install_exec_files() {
     eprintln!("Snapshot");
     insta::assert_debug_snapshot!(store_files);
 
-    drop(root); // cleanup
+    drop((root, mock_instance)); // cleanup
 }
 
 #[test]
 fn should_install_index_files() {
     let CommandTempCwd { pacquet, root, workspace, npmrc_info, .. } =
-        CommandTempCwd::init().add_default_npmrc();
-    let AddDefaultNpmrcInfo { store_dir, .. } = npmrc_info;
+        CommandTempCwd::init().add_mocked_registry();
+    let AddMockedRegistry { store_dir, mock_instance, .. } = npmrc_info;
 
     eprintln!("Creating package.json...");
     let manifest_path = workspace.join("package.json");
     let package_json_content = serde_json::json!({
         "dependencies": {
-            "is-odd": "3.0.1",
-        },
-        "devDependencies": {
-            "fast-decode-uri-component": "1.0.1",
+            "@pnpm.e2e/hello-world-js-bin-parent": "1.0.0",
         },
     });
     fs::write(&manifest_path, package_json_content.to_string()).expect("write to package.json");
@@ -138,5 +129,5 @@ fn should_install_index_files() {
     let index_file_contents = index_file_contents(&store_dir);
     insta::assert_yaml_snapshot!(index_file_contents);
 
-    drop(root); // cleanup
+    drop((root, mock_instance)); // cleanup
 }
