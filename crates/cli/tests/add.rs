b90576c60c369bfdc46098b786d0d0b19887fcfe
khai96_ <hvksmr1996@gmail.com>
2023-11-17 02:49:30 +0700

    test(cli/add): use registry-mock

--- crates/cli/tests/add.rs
+++ crates/cli/tests/add.rs
@@ -2,27 +2,28 @@ use assert_cmd::prelude::*;
 use command_extra::CommandExtra;
 use pacquet_package_manifest::{DependencyGroup, PackageManifest};
 use pacquet_testing_utils::{
-    bin::CommandTempCwd,
+    bin::{AddMockedRegistry, CommandTempCwd},
     fs::{get_all_folders, get_filenames_in_folder},
 };
 use pretty_assertions::assert_eq;
 use std::{env, ffi::OsStr, fs, path::PathBuf};
 use tempfile::TempDir;
 
-fn exec_pacquet_in_temp_cwd<Args>(args: Args) -> (TempDir, PathBuf)
+fn exec_pacquet_in_temp_cwd<Args>(args: Args) -> (TempDir, PathBuf, AddMockedRegistry)
 where
     Args: IntoIterator,
     Args::Item: AsRef<OsStr>,
 {
-    let CommandTempCwd { pacquet, root, workspace, .. } =
-        CommandTempCwd::init().add_default_npmrc();
+    let CommandTempCwd { pacquet, root, workspace, npmrc_info, .. } =
+        CommandTempCwd::init().add_mocked_registry();
     pacquet.with_args(args).assert().success();
-    (root, workspace)
+    (root, workspace, npmrc_info)
 }
 
 #[test]
 fn should_install_all_dependencies() {
-    let (root, workspace) = exec_pacquet_in_temp_cwd(["add", "is-even"]);
+    let (root, workspace, anchor) =
+        exec_pacquet_in_temp_cwd(["add", "@pnpm.e2e/hello-world-js-bin-parent"]);
 
     eprintln!("Directory list");
     insta::assert_debug_snapshot!(get_all_folders(&workspace));
@@ -37,25 +38,29 @@ fn should_install_all_dependencies() {
     eprintln!("Ensure virtual store dir ({virtual_store_dir:?}) exists");
     assert!(virtual_store_dir.exists());
 
-    eprintln!("Ensure that is-buffer does not have any dependencies");
-    let is_buffer_path = virtual_store_dir.join("is-buffer@1.1.6/node_modules");
-    assert_eq!(get_filenames_in_folder(&is_buffer_path), vec!["is-buffer"]);
+    eprintln!("Ensure that @pnpm.e2e/hello-world-js-bin has no other dependencies than itself");
+    let path = virtual_store_dir.join("@pnpm.e2e+hello-world-js-bin@1.0.0/node_modules");
+    assert_eq!(get_filenames_in_folder(&path), ["@pnpm.e2e"]);
+    assert_eq!(get_filenames_in_folder(&path.join("@pnpm.e2e")), ["hello-world-js-bin"]);
 
-    eprintln!("Ensure that is-even have correct dependencies");
-    let is_even_path = virtual_store_dir.join("is-even@1.0.0/node_modules");
-    assert_eq!(get_filenames_in_folder(&is_even_path), vec!["is-even", "is-odd"]);
+    eprintln!("Ensure that @pnpm.e2e/hello-world-js-bin-parent has correct dependencies");
+    let path = virtual_store_dir.join("@pnpm.e2e+hello-world-js-bin-parent@1.0.0/node_modules");
+    assert_eq!(get_filenames_in_folder(&path), ["@pnpm.e2e"]);
+    assert_eq!(
+        get_filenames_in_folder(&path.join("@pnpm.e2e")),
+        ["hello-world-js-bin", "hello-world-js-bin-parent"],
+    );
 
-    eprintln!("Ensure that is-number does not have any dependencies");
-    let is_number_path = virtual_store_dir.join("is-number@3.0.0/node_modules");
-    assert_eq!(get_filenames_in_folder(&is_number_path), vec!["is-number", "kind-of"]);
-
-    drop(root); // cleanup
+    drop((root, anchor)); // cleanup
 }
 
 #[test]
 #[cfg(unix)]
 pub fn should_symlink_correctly() {
-    let (root, workspace) = exec_pacquet_in_temp_cwd(["add", "is-odd"]);
+    use pipe_trait::Pipe;
+
+    let (root, workspace, anchor) =
+        exec_pacquet_in_temp_cwd(["add", "@pnpm.e2e/hello-world-js-bin-parent"]);
 
     eprintln!("Directory list");
     insta::assert_debug_snapshot!(get_all_folders(&workspace));
@@ -72,38 +77,60 @@ pub fn should_symlink_correctly() {
 
     eprintln!("Make sure the symlinks are correct");
     assert_eq!(
-        fs::read_link(virtual_store_dir.join("is-odd@3.0.1/node_modules/is-number")).unwrap(),
-        fs::canonicalize(virtual_store_dir.join("is-number@6.0.0/node_modules/is-number")).unwrap(),
+        virtual_store_dir
+            .join("@pnpm.e2e+hello-world-js-bin-parent@1.0.0")
+            .join("node_modules")
+            .join("@pnpm.e2e")
+            .join("hello-world-js-bin")
+            .pipe(fs::read_link)
+            .expect("read link"),
+        virtual_store_dir
+            .join("@pnpm.e2e+hello-world-js-bin@1.0.0")
+            .join("node_modules")
+            .join("@pnpm.e2e")
+            .join("hello-world-js-bin")
+            .pipe(fs::canonicalize)
+            .expect("canonicalize link target"),
     );
 
-    drop(root); // cleanup
+    drop((root, anchor)); // cleanup
 }
 
 #[test]
 fn should_add_to_package_json() {
-    let (root, dir) = exec_pacquet_in_temp_cwd(["add", "is-odd"]);
+    let (root, dir, anchor) = exec_pacquet_in_temp_cwd(["add", "@pnpm.e2e/hello-world-js-bin"]);
     let file = PackageManifest::from_path(dir.join("package.json")).unwrap();
-    eprintln!("Ensure is-odd is added to package.json#dependencies");
-    assert!(file.dependencies([DependencyGroup::Prod]).any(|(k, _)| k == "is-odd"));
-    drop(root); // cleanup
+    eprintln!("Ensure @pnpm.e2e/hello-world-js-bin is added to package.json#dependencies");
+    assert!(file
+        .dependencies([DependencyGroup::Prod])
+        .any(|(k, _)| k == "@pnpm.e2e/hello-world-js-bin"));
+    drop((root, anchor)); // cleanup
 }
 
 #[test]
 fn should_add_dev_dependency() {
-    let (root, dir) = exec_pacquet_in_temp_cwd(["add", "is-odd", "--save-dev"]);
+    let (root, dir, anchor) =
+        exec_pacquet_in_temp_cwd(["add", "@pnpm.e2e/hello-world-js-bin", "--save-dev"]);
     let file = PackageManifest::from_path(dir.join("package.json")).unwrap();
-    eprintln!("Ensure is-odd is added to package.json#devDependencies");
-    assert!(file.dependencies([DependencyGroup::Dev]).any(|(k, _)| k == "is-odd"));
-    drop(root); // cleanup
+    eprintln!("Ensure @pnpm.e2e/hello-world-js-bin is added to package.json#devDependencies");
+    assert!(file
+        .dependencies([DependencyGroup::Dev])
+        .any(|(k, _)| k == "@pnpm.e2e/hello-world-js-bin"));
+    drop((root, anchor)); // cleanup
 }
 
 #[test]
 fn should_add_peer_dependency() {
-    let (root, dir) = exec_pacquet_in_temp_cwd(["add", "is-odd", "--save-peer"]);
+    let (root, dir, anchor) =
+        exec_pacquet_in_temp_cwd(["add", "@pnpm.e2e/hello-world-js-bin", "--save-peer"]);
     let file = PackageManifest::from_path(dir.join("package.json")).unwrap();
-    eprintln!("Ensure is-odd is added to package.json#devDependencies");
-    assert!(file.dependencies([DependencyGroup::Dev]).any(|(k, _)| k == "is-odd"));
-    eprintln!("Ensure is-odd is added to package.json#peerDependencies");
-    assert!(file.dependencies([DependencyGroup::Peer]).any(|(k, _)| k == "is-odd"));
-    drop(root); // cleanup
+    eprintln!("Ensure @pnpm.e2e/hello-world-js-bin is added to package.json#devDependencies");
+    assert!(file
+        .dependencies([DependencyGroup::Dev])
+        .any(|(k, _)| k == "@pnpm.e2e/hello-world-js-bin"));
+    eprintln!("Ensure @pnpm.e2e/hello-world-js-bin is added to package.json#peerDependencies");
+    assert!(file
+        .dependencies([DependencyGroup::Peer])
+        .any(|(k, _)| k == "@pnpm.e2e/hello-world-js-bin"));
+    drop((root, anchor)); // cleanup
 }
