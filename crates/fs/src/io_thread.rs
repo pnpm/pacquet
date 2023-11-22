7d7046ffae38e252db228c023485f1ef5ceb45b0
khai96_ <hvksmr1996@gmail.com>
2023-11-23 00:21:59 +0700

    feat: IoTask::EnsureFile

--- crates/fs/src/io_thread.rs
+++ crates/fs/src/io_thread.rs
@@ -1,5 +1,7 @@
+use crate::{ensure_file, EnsureFileError};
 use derive_more::{Display, Error};
 use pacquet_task_queue::{Task, TaskQueue};
+use std::path::PathBuf;
 
 /// Dedicated thread for I/O operations.
 pub type IoThread = TaskQueue<IoTask>;
@@ -7,15 +9,23 @@ pub type IoThread = TaskQueue<IoTask>;
 /// Operation to run on [`IoThread`].
 #[derive(Debug)]
 #[non_exhaustive]
-pub enum IoTask {}
+pub enum IoTask {
+    EnsureFile { file_path: PathBuf, content: Vec<u8>, mode: Option<u32> },
+}
 
 #[derive(Debug, Display, Error)]
 #[non_exhaustive]
-pub enum IoTaskError {}
+pub enum IoTaskError {
+    EnsureFile(EnsureFileError),
+}
 
 impl Task for IoTask {
     type Output = Result<(), IoTaskError>;
     fn run(self) -> Self::Output {
-        match self {}
+        match self {
+            IoTask::EnsureFile { file_path, content, mode } => {
+                ensure_file(&file_path, &content, mode).map_err(IoTaskError::EnsureFile)
+            }
+        }
     }
 }
