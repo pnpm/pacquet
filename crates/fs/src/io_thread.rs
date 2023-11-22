b1762ee1a4a63635c13799d5478a36b566157d2b
khai96_ <hvksmr1996@gmail.com>
2023-11-23 00:12:39 +0700

    refactor: rename Operation to IoTask

--- crates/fs/src/io_thread.rs
+++ crates/fs/src/io_thread.rs
@@ -2,14 +2,14 @@ use pacquet_task_queue::{Task, TaskQueue};
 use std::io;
 
 /// Dedicated thread for I/O operations.
-pub type IoThread = TaskQueue<Operation>;
+pub type IoThread = TaskQueue<IoTask>;
 
 /// Operation to run on [`IoThread`].
 #[derive(Debug)]
 #[non_exhaustive]
-pub enum Operation {}
+pub enum IoTask {}
 
-impl Task for Operation {
+impl Task for IoTask {
     type Output = io::Result<()>;
     fn run(self) -> Self::Output {
         match self {}
